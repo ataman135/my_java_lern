5133c1c50c8d5ced045966a2761dbbf3a287aa80
ataman135 <vovasolod85@mail.ru>
2024-06-12 18:11:15 +0300

    Проверка работы проекта с Git

---
+++ test/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int a = 17;
+        int b = 22;
+
+        System.out.println(a + b);
+    }
+}
